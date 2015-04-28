9d876fb1d5ea702957f6712a2e546f24b29806f2
Jin Kwon <onacit@gmail.com>
2015-04-28 23:17:55 +0900

    timestamp builder and nonce builder added

---
+++ src/main/java/com/github/jinahya/rfc5849/NonceBuilder.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2015 Jin Kwon &lt;jinahya at gmail.com&gt;.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+package com.github.jinahya.rfc5849;
+
+
+import java.security.SecureRandom;
+import java.util.Random;
+
+
+/**
+ *
+ * @author Jin Kwon &lt;jinahya at gmail.com&gt;
+ */
+public final class NonceBuilder {
+
+
+    public static NonceBuilder newInstance(final Random random) {
+
+        return new NonceBuilder(random);
+    }
+
+
+    public static NonceBuilder newInstance() {
+
+        return newInstance(new SecureRandom());
+    }
+
+
+    private NonceBuilder(final Random random) {
+
+        super();
+
+        if (random == null) {
+            throw new NullPointerException("null random");
+        }
+
+        this.random = random;
+    }
+
+
+    public String build() {
+
+        return Long.toString((System.currentTimeMillis() << 20)
+                             | random.nextInt(1048576));
+    }
+
+
+    private final Random random;
+
+
+}
+
